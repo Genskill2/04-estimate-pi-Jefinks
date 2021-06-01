269c2f42b729ad73d2de9c21eeb709c850c1cef9
Jefinks <85078274+Jefinks@users.noreply.github.com>
2021-06-01 21:49:55 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -34,14 +34,3 @@ float wallis_pi(int i)
  pi = k*2;
  return pi;
 }
-float wallis_pi(int i)
-{ 
- float k=1;
- float pi;
- for(int n=1;n<=i;n++)
- { float m= (float) n;
-     k=k*(4*m*m/((4*m*m)-1));
- }
- pi = k*2;
- return pi;
-}
